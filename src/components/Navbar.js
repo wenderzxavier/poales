81ee2b4ddfece655308ec20433cbbb6487231272
faber <faber@hivesys.com.br>
2018-10-09 23:13:03 -0300

    feat: implement navigation bar
    This commit comprises the inicla commit boostraped with create-react-app.
    Some file moving was performed for better understanding of the project.
    The navigation bar implemented consists of four links: the project logo
    link, and three other anchoring for sections of the page which will be
    implemented.

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from '../img/leaf.svg';
+import '../styles/Navbar.css';
+
+const Navbar = () => (
+    <header>
+        <nav className='nav-bar'>
+            <div className='navbar-logo'>
+                <a id='homepage-link' href='http://localhost:3000/#' alt='Link to Homepage'>
+                    <img id='logo' src={logo} alt='Leaf Icon' />
+                    <h1 id='logo-text'>POALES</h1>
+                </a>
+            </div>
+            <a className='navbar-link' href='http://localhost:3000/#'>The Project</a>
+            <a className='navbar-link' href='http://localhost:3000/#'>Team</a>
+            <a className='navbar-link' href='http://localhost:3000/#'>Partners</a>
+        </nav>
+    </header>
+)
+
+export default Navbar;
