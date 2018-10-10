3f6382f6595bccf43873ed13fb523226f19e089c
faber <faber@hivesys.com.br>
2018-10-10 13:50:17 -0300

    feat: add cover section
    implement cover section using background video. All styles are saved
    in the cover.css file.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import Navbar from './Navbar';
+import Cover from './Cover';
 
 class App extends Component {
   render() {
     return (
-      <Navbar />
+      <div>
+        <header>
+          <Navbar />
+        </header>
+        <Cover />
+      </div>
     );
   }
 }
