a57ded76b8b29b775d2cd5c04d9691f2a492612f
faber <faber@hivesys.com.br>
2018-10-10 14:36:34 -0300

    style: add responsiveness to navbar and effects
    navbar changes display when screen widht smaller than 600px. navbar
    changes color and display when scroll.

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import backgroundVideoMP4 from '../img/poalis.mp4';
 import backgroundVideoOGG from '../img/poalis.ogg';
 import '../styles/Cover.css';
 
-class Cover extends Component {
-    render() {
-        return (
-            <section>
-                <video className='cover' id='video' autoPlay loop muted>
-                    <source src={backgroundVideoMP4} type='video/mp4' />
-                    <source src={backgroundVideoOGG} type='video/ogg' />
-                    Sorry, but your browser does not support the video background.
+const Cover = () => (
+    <section>
+        <video className='cover' id='video' autoPlay loop muted>
+            <source src={backgroundVideoMP4} type='video/mp4' />
+            <source src={backgroundVideoOGG} type='video/ogg' />
+            Sorry, but your browser does not support the video background.
                 </video>
-                <div className='cover' id='filter'>
-                    <div>
-                        <header id='heading'>POALES</header>
-                        <p id='description'>Connecting local farmers to global buyers</p>
-                    </div>
-                    <div id='scroll-down'>
-                        <div id='mouse'><div id='scroll'></div></div>
-                        <p>Scroll To Learn</p>
-                    </div>
-                </div>
-            </section>
-        )
-    }
-}
+        <div className='cover' id='filter'>
+            <div>
+                <header id='heading'>POALES</header>
+                <p id='description'>Connecting local farmers to global buyers</p>
+            </div>
+            <div id='scroll-down'>
+                <div id='mouse'><div id='scroll'></div></div>
+                <p>Scroll To Learn</p>
+            </div>
+        </div>
+    </section>
+)
 
 export default Cover;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import logo from '../img/leaf.svg';
 import '../styles/Navbar.css';
 
-const Navbar = () => (
-    <nav className='nav-bar'>
-        <div className='navbar-logo'>
-            <a id='homepage-link' href='http://localhost:3000/#' alt='Link to Homepage'>
-                <img id='logo' src={logo} alt='Leaf Icon' />
-                <h1 id='logo-text'>POALES</h1>
-            </a>
-        </div>
-        <a className='navbar-link' href='http://localhost:3000/#'>The Project</a>
-        <a className='navbar-link' href='http://localhost:3000/#'>Team</a>
-        <a className='navbar-link' href='http://localhost:3000/#'>Partners</a>
-    </nav>
-)
+class Navbar extends Component {
+    changeNavColor = (color, display) => {
+        document.getElementById('nav-bar').style.backgroundColor = color;
+        document.getElementById('nav-bar').style.transition = '1.5s';
+        document.getElementById('homepage-link').style.display = display;
+    }
+
+    componentDidMount() {
+        window.onscroll = () => {
+            (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) ?
+            this.changeNavColor('black', 'flex') : this.changeNavColor('transparent', 'none')
+        }
+    }
+
+    render() {
+        return (
+            <nav id='nav-bar'>
+                <div className='navbar-logo'>
+                    <a id='homepage-link' href='http://localhost:3000/#' alt='Link to Homepage'>
+                        <img id='logo' src={logo} alt='Leaf Icon' />
+                        <h1 id='logo-text'>POALES</h1>
+                    </a>
+                </div>
+                <a className='navbar-link' href='http://localhost:3000/#'>The Project</a>
+                <a className='navbar-link' href='http://localhost:3000/#'>Team</a>
+                <a className='navbar-link' href='http://localhost:3000/#'>Partners</a>
+            </nav>
+        )
+    }
+}
 
 export default Navbar;
