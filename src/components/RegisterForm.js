862e54a9ab19e0820e34703152efa07b8ef9e7f5
Wender Xavier <wender.zacariasxavier@gmail.com>
2018-10-22 21:35:41 -0300

    style: register page
    continue stylying register page. Separate produt data to a single file
    on utils folder

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,103 +1,95 @@
 import React from 'react'
+import Products from '../utils/Products'
 import '../styles/RegisterForm.css'
 
 const RegisterForm = () => (
-    <section id='register-background'>
+    <section id='register-background' className='background-image'>
         <form action='/' className='register-form'>
-            <div className='overview-data'>
-                <legend>
-                    Registration Form
+            <legend className='form-title'>
+                Registration Form
             </legend>
-                <div className='input-text'>
-                    <input type='text' id='nome-empresa' name='nome-empresa' placeholder='Company Name' required />
-                    <input type='text' id='id' name='id' placeholder='Employer ID Number' required />
-                    <input type='text' id='location' name='location' placeholder='Location' required />
-                </div>
-            </div>
-            <div className='product-picker'>
-                <legend>
-                    Select the products you work with:
-                </legend>
-                <div className='product-select'>
-                    <label className='product'>
-                        <p>Honey</p>
-                        <input type='checkbox' id='mel' name='product-type' />
+            <div className='farmer-data flex-column'>
+                <div>
+                    <label for='avatar'>Escolher Avatar
+                    <input type='file' id='avatar' name='avatar' accept='image/png, image/jpeg' />
                     </label>
-                    <input type='checkbox' id='cafe' name='product-type' /> Coffee
-                    <input type='checkbox' id='cachaca' name='product-type' /> Licor
-                    <input type='checkbox' id='laticinios' name='product-type' /> Dairy
-                    <input type='checkbox' id='cotton' name='product-type' /> Cotton
                 </div>
-            </div>
-            <div>
                 <div>
-                    <label for='description'>Describe the product:</label>
+                    <label for='bio'>Bio
+                    <textarea rows='5' columns='15' />
+                    </label>
                 </div>
-                <textarea id='description' rows='10' cols='30' placeholder='Information related to amount produced, type of grains, production time'></textarea>
-            </div>
-            <div>
-                <label for='production'>Monthly Production (specify the metric)
-                    <input type='text' id='production' placeholder='25 gallons' required />
-                </label>
-            </div>
-            <div>
-                <label for='capacity'>Growth capacity (specify the metric)
-                    <input type='text' id='capacity' placeholder='30 gallons' required />
-                </label>
-            </div>
-            <div>
-                <label for='destiny'>Current Buyers:
-                    <input type='text' id='destiny' placeholder='Whom are you selling to? (Countries, companies)' />
-                </label>
-            </div>
-            <div>
-                <label for='avatar'>Escolher Avatar
-                    <input type='file' id='avatar' name='avatar' accept='image/png, image/jpeg' />
-                </label>
-            </div>
-            <div>
-                <label for='photos'>Production Photos:
+                <div>
+                    <label for='photos'>Production Photos:
                     <input type='file' id='photos' name='photos' accept='image/png, image/jpeg' multiple />
-                </label>
-            </div>
-            <div>
-                <label for='video'>Cover Video:
-                    <input type='file' id='video' name='video' accept='video/*' />
-                </label>
-            </div>
-            <div>
-                <label for='bio'>Bio
-                    <textarea rows='5' columns='15' />
-                </label>
-            </div>
-            <div>
-                <div>
-                    Cerfitications:
+                    </label>
                 </div>
-                <label for='mel'>
-                    <input type='checkbox' id='mel' name='certification' /> Certification 1
-                </label>
-                <label for='cafe'>
-                    <input type='checkbox' id='cafe' name='certification' /> Certification 2
-                </label>
-                <label for='cachaca'>
-                    <input type='checkbox' id='cachaca' name='certification' /> Certification 3
-                </label>
-                <label for='laticionios'>
-                    <input type='checkbox' id='laticinios' name='certification' /> Certification 4
-                </label>
+                <div>
+                    <label for='video'>Cover Video:
+                    <input type='file' id='video' name='video' accept='video/*' />
+                    </label>
+                </div>
             </div>
-            <div>
-                <label for='avatar'>Escolher Avatar
-                    <input type='file' id='avatar' name='avatar' accept='image/png, image/jpeg' />
+            <div className='product-data flex-column'>
+                <div className='overview-data'>
+                    <div className='flex-text-fields'>
+                        <input type='text' id='nome-empresa' name='nome-empresa' placeholder='Company Name' className='input-text' required />
+                        <input type='text' id='id' name='id' placeholder='Employer ID Number' className='input-text' required />
+                        <input type='text' id='location' name='location' placeholder='Location' className='input-text' required />
+                    </div>
+                </div>
+                <div className='product-picker'>
+                    <legend>
+                        Select the products you work with:
+                </legend>
+                    <div className='product-select'>
+                        {Products.map((data, key) => (
+                            <label key={key} className='product' htmlFor={data.name.toLowerCase()}>
+                                <img src={data.img} className='product-icon' alt={`${data.name} icon`}></img>
+                                <p>{data.name}</p>
+                                <input type='checkbox' id={data.name.toLowerCase()} name='product-type' />
+                            </label>
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <textarea id='register-textarea' rows='10' cols='15' placeholder={`Tell more about your product:${'\n\n'}Information related to amount produced, type of grains, production time`} className='input-text'></textarea>
+                </div>
+                <div className='flex-text-fields'>
+                    <legend>
+                        Specify the metric for the following fields (e.g. gallons, liters):
+                    </legend>                    
+                <input type='text' id='production' name='production' placeholder='Monthly Production (specify the metric)' className='input-text' required />
+                    <input type='text' id='growth-capacity' name='growth-capacity' placeholder='Growth capacity (specify the metric)' className='input-text' required />
+                    <input type='text' id='destiny' name='destiny' placeholder='Current Buyers:' className='input-text' required />
+                </div>
+                <div>
+                    <div>
+                        Cerfitications:
+                </div>
+                    <label for='mel'>
+                        <input type='checkbox' id='mel' name='certification' /> Certification 1
                 </label>
-            </div>
-            <div>
-                <label for='veracity'>Bio
-                    <textarea rows='5' columns='15' />
+                    <label for='cafe'>
+                        <input type='checkbox' id='cafe' name='certification' /> Certification 2
                 </label>
+                    <label for='cachaca'>
+                        <input type='checkbox' id='cachaca' name='certification' /> Certification 3
+                </label>
+                    <label for='laticionios'>
+                        <input type='checkbox' id='laticinios' name='certification' /> Certification 4
+                </label>
+                </div>
+
             </div>
-            <input type='submit' name='sumit' value='Submit' />
+            <div className='terms-conditions'>
+                <div>
+                    <label htmlFor='agreement'>
+                        <input type='checkbox' id='agreement' name='agreement' />I agree to the terms and conditions.
+                    </label>
+                </div>
+                <input type='submit' name='sumit' value='Submit' />
+            </div>
         </form>
     </section>
 )
---
+++ src/utils/Products.js
@@ -0,0 +1,30 @@
+import honey from '../img/honey.svg'
+import beer from '../img/beer.svg'
+import cotton from '../img/cotton.svg'
+import dairy from '../img/dairy.svg'
+import coffee from '../img/coffee.svg'
+
+const Products = [
+    {
+        name: 'Coffee',
+        img: coffee,
+    },
+    {
+        name: 'Honey',
+        img: honey,
+    },
+    {
+        name: 'Licor',
+        img: beer,
+    },
+    {
+        name: 'Cotton',
+        img: cotton,
+    },
+    {
+        name: 'Dairy',
+        img: dairy,
+    },
+]
+
+export default Products
